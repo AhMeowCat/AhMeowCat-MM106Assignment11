95dfef001e0e90b404c0c4ba61ac2521025ef541
CC <99698564+AhMeowCat@users.noreply.github.com>
2022-04-17 20:02:54 -0700

    Add files via upload

--- js/windowSize.js
+++ js/windowSize.js
@@ -5,11 +5,17 @@ onload = function getSize() {
   let h = this.document.documentElement.clientHeight;
   this.document.getElementById("wh").innerHTML = "<h1>Screen Size:<br><br>Width= " + w + " px<br>" + "Height= " + h + "  px<h1>"
   this.window.addEventListener("resize", getSize);
+
+  let title = this.document.getElementById("title");
+  
   if (w < 1200 && w >= 600) {
     this.document.body.style.backgroundColor = "#42789e";
+    title.textContent = "Background color for screen width between 600px & 1200px";
   } else if (w < 600) {
     this.document.body.style.backgroundColor = "#967bb6";
+    title.textContent = "Background color for screen width of less than 600px";
   } else if (w >= 1200) {
     this.document.body.style.backgroundColor = "rgb(32, 117, 89)";
+    title.textContent = "Background color for screen width at or more than 1200px";
   }
 }
